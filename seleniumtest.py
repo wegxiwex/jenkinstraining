d0019398a7f9500c3312ee9513dbe685ec696a40
wegxiwex <iwanprayitno182@gmail.com>
2021-08-20 10:43:12 +0700

    second commit

--- seleniumtest.py
+++ seleniumtest.py
@@ -12,7 +12,7 @@ def test_form_entry():
 	driver.find_element_by_name("nama").send_keys("iwan prayitno")
 	driver.find_element_by_name("nik").send_keys("15093613")
 	driver.find_element_by_name("alamat").send_keys("Pochinki")
-	driver.find_element_by_name("perusahaan").send_keys("PT. SAT")
+	driver.find_element_by_name("perusahaan").send_keys("PT. SAT, Tbk")
 	driver.find_element_by_name("tanggal").send_keys("26 Juni 1993")
 	driver.find_element_by_name("divisi").send_keys("IT")
 	driver.find_element_by_name("posisi").send_keys("Data Mining Programming Analyst")
